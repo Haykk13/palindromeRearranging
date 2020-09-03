e62f45ee53d3a5b089526006bab2b927a9b1e894
Hayk <hayk.hayr3@gmail.com>
2020-09-03 16:33:53 +0400

    Add project files.

---
+++ palindromeRearranging/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace palindromeRearranging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a word: ");
+            string inputString = Console.ReadLine();
+            if (palindromeRearranging(inputString))
+            {
+                Console.WriteLine("The word is Palindrome Rearranging!");
+            }
+            else
+            {
+                Console.WriteLine("The word is NOT Palindrome Rearranging!");
+            }
+            Console.ReadLine();
+        }
+        static bool palindromeRearranging(string inputString)
+        {
+            bool isPalindrome = true;
+            string ch = "";
+            int num = 0;
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                int count = 1;
+                for (int j = 0; j < inputString.Length; j++)
+                {
+                    if(i != j)
+                    {
+                        if(inputString[i] == inputString[j])
+                        {
+                            count++;
+                        }
+                    }
+                }
+                if(count % 2 == 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    num++;
+                    if (num == 1)
+                    {
+                        ch += inputString[i];
+                    }
+                    else
+                    {
+                        if (ch[0] != inputString[i])
+                        {
+                            isPalindrome = false;
+                            break;
+                        }
+                    }
+                }
+            }
+            return isPalindrome;
+        }
+
+    }
+}
